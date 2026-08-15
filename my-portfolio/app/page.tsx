880bca6d2ba78592366c62389c7ca54d5aa5c853
Maria Luiza Pestana <pestanamaria099@gmail.com>
2026-08-15 16:12:58 -0300

    https://github.com/malupestana/Portfolio-da-Malu.git

---
+++ my-portfolio/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      
+            <Image
+              className="dark:invert h-[14px] w-4"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={14}
+            />
+          
+      </main>
+  );
+}
