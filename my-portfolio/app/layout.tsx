008746d732fd7c3c924febc95259b2fa8d09f774
Maria Luiza Pestana <pestanamaria099@gmail.com>
2026-08-31 15:12:58 -0300

    Fix: preview link

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.scss";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://marialuiza-dev.vercel.app'),
   title: "Maria | Desenvolvedora Backend",
   description: "Portfólio de Maria, Desenvolvedora Backend.",
 };
